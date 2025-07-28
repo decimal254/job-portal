f40c3313d57205ba8aafcba91fa1b419c7090af3
decimal254 <dennow2030@gmail.com>
2025-07-28 15:28:04 +0300

    Add register.php landing page for Job Seeker and Employer

---
+++ register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <div class="form-box">
+        <h1>Create Your Account</h1>
+        <P>Please choose Your registration type:</P>
+        <a href="auth/register_jobseeker.php" class="btn">Register as a Job Seeker</a>
+        <a href="auth/register_employer.php" class="btn">Register as a Employer</a>
+        <p>Already have an Account? <a href="auth login.php">Login here</p>
+    </div>
+</head>
+<body>
+    
+</body>
+</html>
